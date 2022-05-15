aead3bcfb8346ffdfeba55974f6f6e26fa2f7b0f
SipheMkhosi <siphemkhosi7@gmail.com>
2022-05-15 10:21:26 +0200

    edited textbill

--- js/text-bill.js
+++ js/text-bill.js
@@ -8,39 +8,42 @@ const totalBillTotalElem = document.querySelector(".totalOne");
 
 
 //add an event listener for when the add button is pressed
-var roundedBillTotal3 = 0;
 
-var callTotal = 0;
-var smsTotal = 0;
 
+var roundedBillTotal3t = 0;
+   
+var callTotalt = 0;
+var smsTotalt = 0;
 
 function calculateBtnClicked2(){
-
+   
+  
  const aButtonText = document.querySelector(".billTypeText");
 if(aButtonText){
     var billString =  aButtonText.value.trim();
 }
-if (billString = "call"){
-    callTotal += 2.75;
-}
-else if (billString = "sms"){
-    smsTotal += 0.75;
+
+if(billString === "call"){
+    callTotalt += 2.75;
+}else if(billString === "sms"){
+    smsTotalt += 0.75;
 
 }
-var roundedBillTotal1 =   callTotal.toFixed(2);
-callBillTotalElem.innerHTML = roundedBillTotal1;
 
-var roundedBillTotal2 =   smsTotal.toFixed(2);
-smsBillTotalElem.innerHTML = roundedBillTotal2;
+var roundedBillTotal1t =   callTotalt.toFixed(2);
+callBillTotalElem.innerHTML = roundedBillTotal1t;
+
+var roundedBillTotal2t =   smsTotalt.toFixed(2);
+smsBillTotalElem.innerHTML = roundedBillTotal2t;
 
 
-roundedBillTotal3 = Number(roundedBillTotal1) + Number(roundedBillTotal2)
-totalBillTotalElem.innerHTML = roundedBillTotal3.toFixed(2);
+roundedBillTotal3t = Number(roundedBillTotal1t) + Number(roundedBillTotal2t)
+totalBillTotalElem.innerHTML = roundedBillTotal3t.toFixed(2);
 
-//000000000000000
-if(roundedBillTotal3>30 && roundedBillTotal3<50){
+
+if(roundedBillTotal3t>=30 && roundedBillTotal3t<50){
     totalBillTotalElem.classList.add("warning");
-}else if(roundedBillTotal3>50){
+}else if(roundedBillTotal3t>50){
     totalBillTotalElem.classList.add("danger");
 }
 
